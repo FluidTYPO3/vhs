bd3d269bf3cf5edc5ea2d1838de8498f3598af60
Claus Due <claus@wildside.dk>
2012-11-02 13:40:38 +0100

    [TASK] Base extension files

---
+++ ext_emconf.php
@@ -0,0 +1,40 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'VHS: Fluid ViewHelpers',
+	'description' => 'A collection of ViewHelpers to perform rendering tasks which are not natively supported by Fluid - for example: advanced formatters, math calculators, specialized conditions and Iterator/Array calculators and processors',
+	'category' => 'misc',
+	'author' => 'Claus Due',
+	'author_email' => 'claus@wildside.dk',
+	'author_company' => 'Wildside A/S',
+	'shy' => '',
+	'dependencies' => 'cms,extbase,fluid',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'version' => '1.0.0',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5-0.0.0',
+			'cms' => '',
+			'extbase' => '',
+			'fluid' => '',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => '',
+);
+
+?>
