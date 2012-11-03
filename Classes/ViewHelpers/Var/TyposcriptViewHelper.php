bc2798f026c105e2259cbaa5f76f74600aae3ed6
Claus Due <claus@wildside.dk>
2012-11-03 14:32:01 +0100

    [FEATURE] Typoscript Var ViewHelper

---
+++ Classes/ViewHelpers/Var/TyposcriptViewHelper.php
@@ -0,0 +1,101 @@
+<?php
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2012 Claus Due <claus@wildside.dk>, Wildside A/S
+*
+*  All rights reserved
+*
+*  This script is part of the TYPO3 project. The TYPO3 project is
+*  free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation; either version 2 of the License, or
+*  (at your option) any later version.
+*
+*  The GNU General Public License can be found at
+*  http://www.gnu.org/copyleft/gpl.html.
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ * Accesses Typoscript paths
+ *
+ * @author Claus Due <claus@wildside.dk>, Wildside A/S
+ * @package Vhs
+ * @subpackage ViewHelpers\Var
+ */
+class Tx_Vhs_ViewHelpers_TyposcriptViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+	/**
+	 * @var Tx_Extbase_Configuration_ConfigurationManagerInterface
+	 */
+	protected $configurationManager;
+
+	/**
+	 * @param Tx_Extbase_Configuration_ConfigurationManagerInterface $configurationManager
+	 * @return void
+	 */
+	public function injectConfigurationManager(Tx_Extbase_Configuration_ConfigurationManagerInterface $configurationManager) {
+		$this->configurationManager = $configurationManager;
+	}
+
+	/**
+	 * Render
+	 *
+	 * @param string $path
+	 * @return mixed
+	 */
+	public function render($path = NULL) {
+		if ($path === NULL) {
+			$path = $this->renderChildren();
+		}
+		if (!$path) {
+			return NULL;
+		}
+		$all = $this->configurationManager->getConfiguration(Tx_Extbase_Configuration_ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
+		$segments = explode('.', $path);
+		$value = $all;
+		foreach ($segments as $path) {
+			if (isset($value[$path . '.'])) {
+				$value = $value[$path . '.'];
+			} else {
+				$value = $value[$path];
+			}
+		}
+		if (is_array($value)) {
+			$value = $this->convertTypoScriptArrayToPlainArray($value);
+		}
+		return $value;
+	}
+
+	/**
+	 * @param array $typoScriptArray
+	 * @return array
+	 */
+	protected function convertTypoScriptArrayToPlainArray(array $typoScriptArray) {
+		foreach ($typoScriptArray as $key => &$value) {
+			if (substr($key, -1) === '.') {
+				$keyWithoutDot = substr($key, 0, -1);
+				$hasNodeWithoutDot = array_key_exists($keyWithoutDot, $typoScriptArray);
+				$typoScriptNodeValue = $hasNodeWithoutDot ? $typoScriptArray[$keyWithoutDot] : NULL;
+				if (is_array($value)) {
+					$typoScriptArray[$keyWithoutDot] = $this->convertTypoScriptArrayToPlainArray($value);
+					if (!is_null($typoScriptNodeValue)) {
+						$typoScriptArray[$keyWithoutDot]['_typoScriptNodeValue']  = $typoScriptNodeValue;
+					}
+					unset($typoScriptArray[$key]);
+				} else {
+					$typoScriptArray[$keyWithoutDot] = NULL;
+				}
+			}
+		}
+		return $typoScriptArray;
+	}
+
+}
