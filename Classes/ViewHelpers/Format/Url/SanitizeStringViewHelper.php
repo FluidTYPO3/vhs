ee2a6166deab26ac9c7a4d152030b9f0578da38b
Claus Due <claus@wildside.dk>
2012-11-10 21:59:00 +0100

    [DOC] Updated description of Format / Url / SanitizeString

--- Classes/ViewHelpers/Format/Url/SanitizeStringViewHelper.php
+++ Classes/ViewHelpers/Format/Url/SanitizeStringViewHelper.php
@@ -29,6 +29,24 @@
  * further processing. For example, the text "I am Mr. Brown,
  * how are you?" becomes "i-am-mr-brown-how-are-you".
  *
+ * Also useful when creating anchor link names, for example
+ * for news entries in your custom EXT:news list template, in
+ * which case each news item's title would become an anchor:
+ *
+ * <a name="{newsItem.title -> v:format.url.sanitizeString()}"></a>
+ *
+ * And links would look much like the detail view links:
+ *
+ * /news/#this-is-a-newsitem-title
+ *
+ * When used with list views it has the added benefit of not
+ * breaking if the item referenced is removed, it can be read
+ * by Javascript (for example to dynamically expand the news
+ * item being referenced). The sanitized urls are also ideal
+ * to use for AJAX based detail views - and in almot all cases
+ * the sanitized string will be 100% identical to the one used
+ * by Realurl when translating using table lookups.
+ *
  * @author Claus Due <claus@wildside.dk>, Wildside A/S
  * @package Vhs
  * @subpackage ViewHelpers\Format
