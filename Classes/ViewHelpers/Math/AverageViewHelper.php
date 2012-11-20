fabf932edcba315f02429cb0ae8228163b4df316
Claus Due <claus@wildside.dk>
2012-11-20 21:01:41 +0100

    [FEATURE] Math / Average ViewHelper

---
+++ Classes/ViewHelpers/Math/AverageViewHelper.php
@@ -0,0 +1,86 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Claus Due <claus@wildside.dk>, Wildside A/S
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Math: Average
+ *
+ * Performs average across an array. If $a is an array and
+ * $b is an array, each member of $a is averaged against the
+ * same member in $b. If $a is an array and $b is a number,
+ * each member of $a is averaged agained $b. If $a is an array
+ * this array is averaged to one number. If $a is a number and
+ * $b is not provided or NULL, $a is gracefully returned as an
+ * average value of itself.
+ *
+ * @author Claus Due <claus@wildside.dk>, Wildside A/S
+ * @package Vhs
+ * @subpackage ViewHelpers
+ */
+class Tx_Vhs_ViewHelpers_Math_AverageViewHelper extends Tx_Vhs_ViewHelpers_Math_AbstractMultipleMathViewHelper {
+
+	/**
+	 * @return void
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+		$this->overrideArgument('b', 'mixed', 'Optional: Second number or Iterator/Traversable/Array for calculation', FALSE, NULL);
+	}
+
+	/**
+	 * @return mixed
+	 * @throw Exception
+	 */
+	public function render() {
+		$a = $this->getInlineArgument();
+		$b = $this->arguments['b'];
+		$aIsIterable = $this->assertIsArrayOrIterator($a);
+		$bIsIterable = $this->assertIsArrayOrIterator($b);
+		if ($aIsIterable && $b === NULL) {
+			$a = $this->convertTraversableToArray($a);
+			$sum = array_sum($a);
+			$distribution = count($a);
+			return $sum / $distribution;
+		} elseif ($aIsIterable && $bIsIterable === FALSE) {
+			$a = $this->convertTraversableToArray($a);
+			foreach ($a as $index => $value) {
+				$a[$index] = $this->calculateAction($value, $b);
+			}
+			return $a;
+		} elseif ($a && $b === NULL) {
+			return $a;
+		}
+		return $this->calculate($a, $b);
+	}
+
+	/**
+	 * @param mixed $a
+	 * @param $b
+	 * @return mixed
+	 */
+	protected function calculateAction($a, $b) {
+		return ($a + $b) / 2;
+	}
+
+}
