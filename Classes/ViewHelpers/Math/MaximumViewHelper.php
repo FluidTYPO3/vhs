2f99c3536a2de7ec38a6badd8ab17eaa5b2ac3e9
Claus Due <claus@wildside.dk>
2012-11-24 23:54:47 +0100

    [FEATURE] Math / Maximum ViewHelper

---
+++ Classes/ViewHelpers/Math/MaximumViewHelper.php
@@ -0,0 +1,62 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Claus Due <claus@wildside.dk>, Wildside A/S
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Math: Maximum
+ *
+ * Gets the highest number in array $a or the highest
+ * number of numbers $a and $b.
+ *
+ * @author Claus Due <claus@wildside.dk>, Wildside A/S
+ * @package Vhs
+ * @subpackage ViewHelpers\Math
+ */
+class Tx_Vhs_ViewHelpers_Math_MaximumViewHelper extends Tx_Vhs_ViewHelpers_Math_AbstractMultipleMathViewHelper {
+
+	/**
+	 * @return mixed
+	 * @throw Exception
+	 */
+	public function render() {
+		$a = $this->getInlineArgument();
+		$b = $this->arguments['b'];
+		$aIsIterable = $this->assertIsArrayOrIterator($a);
+		if ($aIsIterable && $b === NULL) {
+			$a = $this->convertTraversableToArray($a);
+			return max($a);
+		}
+		return $this->calculate($a, $b);
+	}
+
+	/**
+	 * @param mixed $a
+	 * @param mixed $b
+	 * @return mixed
+	 */
+	protected function calculateAction($a, $b) {
+		return max($a, $b);
+	}
+
+}
