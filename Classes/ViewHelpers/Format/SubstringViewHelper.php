b745c577ae144d0063784e6fcfaf854ac33e242e
Claus Due <claus@wildside.dk>
2012-11-25 02:56:33 +0100

    [FEATURE] Format / Substring ViewHelper

---
+++ Classes/ViewHelpers/Format/SubstringViewHelper.php
@@ -0,0 +1,53 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Claus Due <claus@wildside.dk>, Wildside A/S
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Gets a substring from a string or string-compatible value
+ *
+ * @author Claus Due <claus@wildside.dk>, Wildside A/S
+ * @package Vhs
+ * @subpackage ViewHelpers\Format
+ */
+class Tx_Vhs_ViewHelpers_Format_SubstringViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+	/**
+	 * Substrings a string or string-compatible value
+	 *
+	 * @param string $content Content string to substring
+	 * @param integer $start Positive or negative offset
+	 * @param integer $length Positive or negative length
+	 * @return string
+	 */
+	public function render($content = NULL, $start = 0, $length = NULL) {
+		if ($content === NULL) {
+			$content = $this->renderChildren();
+		}
+		if ($length !== NULL) {
+			return substr($content, $start, $length);
+		}
+		return substr($content, $start);
+	}
+
+}
