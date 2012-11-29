cd3e4b67f14693349bb68a6de5f9ba74265bad6d
Claus Due <claus@wildside.dk>
2012-11-29 21:32:05 +0100

    [FEATURE] Format / Replace ViewHelper
    Interface for str_replace with default replacement value being an empty string.

---
+++ Classes/ViewHelpers/Format/ReplaceViewHelper.php
@@ -0,0 +1,49 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Claus Due <claus@wildside.dk>, Wildside A/S
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Replaces $substring in $content with $replacement.
+ *
+ * @author Claus Due <claus@wildside.dk>, Wildside A/S
+ * @package Vhs
+ * @subpackage ViewHelpers\Format
+ */
+class Tx_Vhs_ViewHelpers_Format_ReplaceViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+	/**
+	 * @param string $substring
+	 * @param string $content
+	 * @param string $replacement
+	 * @param integer $count
+	 * @return string
+	 */
+	public function render($substring, $content = NULL, $replacement = '', $count = NULL) {
+		if ($content === NULL) {
+			$content = $this->renderChildren();
+		}
+		return str_replace($substring, $replacement, $content, $count);
+	}
+
+}
