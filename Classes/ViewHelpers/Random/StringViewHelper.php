0b00cb2a6080e10af645c9ec30a5b7a408d056a4
Claus Due <claus@wildside.dk>
2012-12-01 01:54:14 +0100

    [FEATURE] Random / String ViewHelper

---
+++ Classes/ViewHelpers/Random/StringViewHelper.php
@@ -0,0 +1,64 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Claus Due <claus@wildside.dk>, Wildside A/S
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Random String Generator
+ *
+ * Use either "minimumLength"/"maximumLength" or just "length".
+ *
+ * Specify the characters which can be randomized using "characters".
+ *
+ * Has built-in insurance that first character of random string is
+ * an alphabetic character (allowing safe use as DOM id for example).
+ *
+ * @author Claus Due <claus@wildside.dk>, Wildside A/S
+ * @package Vhs
+ * @subpackage ViewHelpers\Random
+ */
+class Tx_Vhs_ViewHelpers_Random_StringViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+	/**
+	 * @param integer $length
+	 * @param integer $minimumLength
+	 * @param integer $maximumLength
+	 * @param string $characters
+	 * @return string
+	 */
+	public function render($length = NULL, $minimumLength = 32, $maximumLength = 32, $characters = '0123456789abcdef') {
+		$minimumLength = intval($minimumLength);
+		$maximumLength = intval($maximumLength);
+		$length = ($minimumLength != $maximumLength ? rand($minimumLength, $maximumLength) : ($length !== NULL ? $length : $minimumLength));
+		$string = '';
+		for ($i = 0; $i < $length && $length > 0; $i++) {
+			$randomIndex = rand(0, strlen($characters));
+			$string .= $characters{$randomIndex};
+		}
+		$characters = preg_replace('/([^a-z]+)/i', '', $characters);
+		$randomIndex = rand(0, strlen($characters) - 1);
+		$string{0} = $characters{$randomIndex};
+		return $string;
+	}
+
+}
