af87717b6f5993db16f0bb68adff9cd28b0a67c3
Bjoern Fromme <mail@bjo3rn.com>
2012-12-31 13:07:19 +0100

    [FEATURE] Added Media\Size ViewHelper

---
+++ Classes/ViewHelpers/Media/SizeViewHelper.php
@@ -0,0 +1,72 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Björn Fromme <fromme@dreipunktnull.com>, dreipunktnull
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+/**
+ * Returns the size of the provided file in bytes
+ *
+ * @author Björn Fromme <fromme@dreipunktnull.com>, dreipunktnull
+ * @package Vhs
+ * @subpackage ViewHelpers\Media
+ */
+class Tx_Vhs_ViewHelpers_Media_SizeViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+    /**
+     * Initialize arguments.
+     *
+     * @return void
+     * @api
+     */
+    public function initializeArguments() {
+        $this->registerArgument('path', 'string', 'Path to the file to determine size for.', TRUE);
+    }
+
+    /**
+     * @return int
+     */
+    public function render() {
+
+        $path = $this->arguments['path'];
+
+        if ($path === NULL) {
+            $path = $this->renderChildren();
+            if ($path === NULL) {
+                return 0;
+            }
+        }
+
+        if (!file_exists($path) || is_dir($path)) {
+            throw new Tx_Fluid_Core_ViewHelper_Exception('Cannot determine size of "' . $path . '". File does not exist or is a directory.', 1356953963);            
+        }
+
+        $size = filesize($path);
+
+        if ($size === FALSE) {
+            throw new Tx_Fluid_Core_ViewHelper_Exception('Cannot determine size of "' . $path . '".', 1356954032);
+        }
+
+        return $size;
+    }
+
+}
---
+++ Tests/Unit/ViewHelpers/Media/SizeViewHelperTest.php
@@ -0,0 +1,79 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Björn Fromme <fromme@dreipunktnull.com>, dreipunktnull
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+/**
+ * @author Björn Fromme <fromme@dreipunktnull.com>, dreipunktnull
+ * @package Vhs
+ */
+class Tx_Vhs_ViewHelpers_Media_SizeViewHelperTest extends Tx_Extbase_Tests_Unit_BaseTestCase {
+
+    /**
+     * @var string
+     */
+    protected $fixturesPath;
+
+    public function setUp() {
+        $this->fixturesPath = __DIR__ . '/../../../Fixtures/Files';
+    }
+
+    /**
+     * @test
+     */
+    public function returnsZeroForEmptyArguments() {
+        $viewHelper = $this->getMock('Tx_Vhs_ViewHelpers_Media_SizeViewHelper', array('renderChildren'));
+        $viewHelper->expects($this->once())->method('renderChildren')->will($this->returnValue(NULL));
+        $this->assertEquals(0, $viewHelper->render());
+    }
+
+    /**
+     * @test
+     */
+    public function returnsFileSizeAsInteger() {
+        $viewHelper = $this->getMock('Tx_Vhs_ViewHelpers_Media_SizeViewHelper', array('renderChildren'));
+        $viewHelper->expects($this->once())->method('renderChildren')->will($this->returnValue($this->fixturesPath . '/typo3_logo.jpg'));
+        $this->assertEquals(7094, $size = $viewHelper->render());
+    }
+
+    /**
+     * @test
+     */
+    public function throwsExceptionWhenFileNotFound() {
+        $viewHelper = $this->getMock('Tx_Vhs_ViewHelpers_Media_SizeViewHelper', array('renderChildren'));
+        $viewHelper->expects($this->once())->method('renderChildren')->will($this->returnValue('/this/path/hopefully/does/not/exist.txt'));
+        $this->setExpectedException('Tx_Fluid_Core_ViewHelper_Exception');
+        $size = $viewHelper->render();
+    }
+
+    /**
+     * @test
+     */
+    public function throwsExceptionWhenFileIsNotAccessibleOrIsADirectory() {
+        $viewHelper = $this->getMock('Tx_Vhs_ViewHelpers_Media_SizeViewHelper', array('renderChildren'));
+        $viewHelper->expects($this->once())->method('renderChildren')->will($this->returnValue($this->fixturesPath));
+        $this->setExpectedException('Tx_Fluid_Core_ViewHelper_Exception');
+        $size = $viewHelper->render();
+    }
+
+}
