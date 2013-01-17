c0e023b7fb66afd124e163f76ed6b438f2a26cb4
Bjoern Fromme <mail@bjo3rn.com>
2013-01-17 18:11:29 +0100

    [FEATURE] Add JSON de-/encode ViewHelpers

---
+++ Classes/ViewHelpers/Format/Json/DecodeViewHelper.php
@@ -0,0 +1,55 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Björn Fromme <fromme@dreipunktnull.com>, dreipunktnull
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+/**
+ * Converts the JSON encoded argument into a PHP variable
+ *
+ * @author Björn Fromme <fromme@dreipunktnull.com>, dreipunktnull
+ * @package Vhs
+ * @subpackage ViewHelpers\Format\Json
+ */
+class Tx_Vhs_ViewHelpers_Format_Json_DecodeViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+	/**
+	 * @param string $json
+	 * @return mixed
+	 */
+	public function render($json = '') {
+		if ('' === $json) {
+			$json = $this->renderChildren();
+			if ('' === $json) {
+				return;
+			}
+		}
+
+		$value = json_decode($json, TRUE);
+
+		if (json_last_error() !== JSON_ERROR_NONE) {
+			throw new Tx_Fluid_Core_ViewHelper_Exception('The provided argument is invalid JSON.', 1358440054);            
+		}
+
+		return $value;
+	}
+}
