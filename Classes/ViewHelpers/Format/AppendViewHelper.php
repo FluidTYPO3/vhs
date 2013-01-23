b551dca9d4d4c19eb739e9b90387decd77d20395
Claus Due <claus@wildside.dk>
2013-01-23 23:29:15 +0100

    [FEATURE] Format / Prepend and Format / Append ViewHelpers

---
+++ Classes/ViewHelpers/Format/AppendViewHelper.php
@@ -0,0 +1,69 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Claus Due <claus@wildside.dk>, Wildside A/S
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * ### Format: Append string content
+ *
+ * Appends a string after another string. Although this task is very
+ * easily done in standard Fluid - i.e. {subject}{add} - this
+ * ViewHelper makes advanced chained inline processing possible:
+ *
+ *     <!-- useful when needing to chain string processing. Remove all "foo" and "bar"
+ *          then add a text containing both "foo" and "bar", then format as HTML -->
+ *     {text -> v:format.eliminate(strings: 'foo,bar')
+ *           -> v:format.append(add: ' - my foo and bar are the only ones in this text.')
+ *           -> f:format.html()}
+ *     <!-- NOTE: you do not have to break the lines; done here only for presentation purposes -->
+ *
+ * Makes no sense used as tag based ViewHelper:
+ *
+ *     <!-- DO NOT USE - depicts COUNTERPRODUCTIVE usage! -->
+ *     <v:format.append add="{f:translate(key: 're')}">{subject}</v:format.append>
+ *     <!-- ... which is the exact same as ... -->
+ *     <f:translate key="re" />{subject} <!-- OR --> {f:translate(key: 're')}{subject}
+ *
+ * In other words: use this only when you do not have the option of
+ * simply using {subject}{add}, i.e. in complex inline statements used
+ * as attribute values on other ViewHelpers (where tag usage is undesirable).
+ *
+ * @author Claus Due <claus@wildside.dk>, Wildside A/S
+ * @package Vhs
+ * @subpackage ViewHelpers\Format
+ */
+class Tx_Vhs_ViewHelpers_Format_AppendViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+	/**
+	 * @param string $subject
+	 * @param string $add
+	 * @return string
+	 */
+	public function render($subject = NULL, $add) {
+		if (NULL === $subject) {
+			$subject = $this->renderChildren();
+		}
+		return $subject . $add;
+	}
+
+}
---
+++ Classes/ViewHelpers/Format/PrependViewHelper.php
@@ -0,0 +1,58 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Claus Due <claus@wildside.dk>, Wildside A/S
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * ### Format: Prepend string content
+ *
+ * Prepends one string on another. Although this task is very
+ * easily done in standard Fluid - i.e. {add}{subject} - this
+ * ViewHelper makes advanced chained inline processing possible:
+ *
+ *     <!-- Adds 1H to DateTime, formats using timestamp input which requires prepended @ -->
+ *     {dateTime.timestamp
+ *         -> v:math.sum(b: 3600)
+ *         -> v:format.prepend(add: '@')
+ *         -> v:format.date(format: 'Y-m-d H:i')}
+ *     <!-- You don't have to break the syntax into lines; done here for display only -->
+ *
+ * @author Claus Due <claus@wildside.dk>, Wildside A/S
+ * @package Vhs
+ * @subpackage ViewHelpers\Format
+ */
+class Tx_Vhs_ViewHelpers_Format_PrependViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper {
+
+	/**
+	 * @param string $subject
+	 * @param string $add
+	 * @return string
+	 */
+	public function render($subject = NULL, $add) {
+		if (NULL === $subject) {
+			$subject = $this->renderChildren();
+		}
+		return $add . $subject;
+	}
+
+}
