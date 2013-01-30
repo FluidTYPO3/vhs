ad650e0447d20c24256935e1d0cd192c495a766d
Bjoern Fromme <mail@bjo3rn.com>
2013-01-30 09:35:59 +0100

    [FEATURE] Add media related base VHs, add image related VH, add video VH

---
+++ Classes/ViewHelpers/Media/AbstractMediaViewHelper.php
@@ -0,0 +1,66 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Björn Fromme <fromme@dreipunktnull.com>, dreipunktnull
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+/**
+ * Base class for media related view helpers
+ *
+ * @author Björn Fromme <fromme@dreipunktnull.com>, dreipunktnull
+ * @package Vhs
+ * @subpackage ViewHelpers\Media
+ */
+class Tx_Vhs_ViewHelpers_Media_AbstractMediaViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractTagBasedViewHelper {
+
+	/**
+	 *
+	 * @var string
+	 */
+	protected $mediaSource;
+
+	/**
+	 * Initialize arguments.
+	 *
+	 * @return void
+	 * @api
+	 */
+	public function initializeArguments() {
+		$this->registerArgument('src', 'string', 'Path to the media resource.', TRUE);
+		$this->registerArgument('relative', 'boolean', 'If FALSE media URIs are rendered absolute. URIs in backend mode are always absolute.', FALSE, TRUE);
+	}
+
+	/**
+	 * Turns a relative source URL into an absolute URL
+	 * if required
+	 *
+	 * @param string $src
+	 * @return string
+	 */
+	public function preprocessSourceUrl($src) {
+		if (TYPO3_MODE === 'BE' || FALSE === (boolean) $this->arguments['relative']) {
+			$src = t3lib_div::getIndpEnv('TYPO3_SITE_URL') . $src;
+		}
+
+		return $src;
+	}
+}
