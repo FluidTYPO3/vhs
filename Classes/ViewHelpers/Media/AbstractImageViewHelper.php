97acfd60b07af0670ebae37b4605c4cbc2debccf
Claus Due <claus@wildside.dk>
2013-02-01 10:13:26 +0100

    [TASK] AbstractImageViewHelper indentations to tabs

--- Classes/ViewHelpers/Media/AbstractImageViewHelper.php
+++ Classes/ViewHelpers/Media/AbstractImageViewHelper.php
@@ -33,135 +33,135 @@
  */
 class Tx_Vhs_ViewHelpers_Media_AbstractImageViewHelper extends Tx_Vhs_ViewHelpers_Media_AbstractMediaViewHelper {
 
-    /**
-     * @var t3lib_fe contains a backup of the current $GLOBALS['TSFE'] if used in BE mode
-     */
-    protected $tsfeBackup;
+	/**
+	 * @var t3lib_fe contains a backup of the current $GLOBALS['TSFE'] if used in BE mode
+	 */
+	protected $tsfeBackup;
 
-    /**
-     * @var string
-     */
-    protected $workingDirectoryBackup;
+	/**
+	 * @var string
+	 */
+	protected $workingDirectoryBackup;
 
-    /**
-     * @var tslib_cObj
-     */
-    protected $contentObject;
+	/**
+	 * @var tslib_cObj
+	 */
+	protected $contentObject;
 
-    /**
-     * @var Tx_Extbase_Configuration_ConfigurationManagerInterface
-     */
-    protected $configurationManager;
+	/**
+	 * @var Tx_Extbase_Configuration_ConfigurationManagerInterface
+	 */
+	protected $configurationManager;
 
-    /**
-     * Result of tslib_cObj::getImgResource()
-     * @var array
-     */
-    protected $imageInfo;
+	/**
+	 * Result of tslib_cObj::getImgResource()
+	 * @var array
+	 */
+	protected $imageInfo;
 
-    /**
-     * @param Tx_Extbase_Configuration_ConfigurationManagerInterface $configurationManager
-     * @return void
-     */
-    public function injectConfigurationManager(Tx_Extbase_Configuration_ConfigurationManagerInterface $configurationManager) {
-        $this->configurationManager = $configurationManager;
-        $this->contentObject = $this->configurationManager->getContentObject();
-    }
+	/**
+	 * @param Tx_Extbase_Configuration_ConfigurationManagerInterface $configurationManager
+	 * @return void
+	 */
+	public function injectConfigurationManager(Tx_Extbase_Configuration_ConfigurationManagerInterface $configurationManager) {
+		$this->configurationManager = $configurationManager;
+		$this->contentObject = $this->configurationManager->getContentObject();
+	}
 
-    /**
-     * Initialize arguments.
-     *
-     * @return void
-     * @api
-     */
-    public function initializeArguments() {
-        parent::initializeArguments();
-        $this->registerArgument('src', 'string', 'Path to the media resource.', TRUE);
-        $this->registerArgument('width', 'int', 'Optional width.', FALSE);
-        $this->registerArgument('height', 'int', 'Optional height.', FALSE);
-    }
+	/**
+	 * Initialize arguments.
+	 *
+	 * @return void
+	 * @api
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+		$this->registerArgument('src', 'string', 'Path to the media resource.', TRUE);
+		$this->registerArgument('width', 'int', 'Optional width.', FALSE);
+		$this->registerArgument('height', 'int', 'Optional height.', FALSE);
+	}
 
-    /**
-     *
-     * @return void
-     */
-    public function preprocessImage() {
-        $src = $this->arguments['src'];
-        $width = $this->arguments['width'];
-        $height = $this->arguments['height'];
-        $minW = $this->arguments['minW'];
-        $minH = $this->arguments['minH'];
-        $maxW = $this->arguments['maxW'];
-        $maxH = $this->arguments['maxH'];
+	/**
+	 *
+	 * @return void
+	 */
+	public function preprocessImage() {
+		$src = $this->arguments['src'];
+		$width = $this->arguments['width'];
+		$height = $this->arguments['height'];
+		$minW = $this->arguments['minW'];
+		$minH = $this->arguments['minH'];
+		$maxW = $this->arguments['maxW'];
+		$maxH = $this->arguments['maxH'];
 
-        if (TYPO3_MODE === 'BE') {
-            $this->simulateFrontendEnvironment();
-        }
+		if (TYPO3_MODE === 'BE') {
+			$this->simulateFrontendEnvironment();
+		}
 
-        $setup = array(
-            'width'  => $width,
-            'height' => $height,
-            'minW'   => $minWidth,
-            'minH'   => $minHeight,
-            'maxW'   => $maxWidth,
-            'maxH'   => $maxHeight
-        );
+		$setup = array(
+			'width'  => $width,
+			'height' => $height,
+			'minW'   => $minWidth,
+			'minH'   => $minHeight,
+			'maxW'   => $maxWidth,
+			'maxH'   => $maxHeight
+		);
 
-        if (TYPO3_MODE === 'BE' && substr($src, 0, 3) === '../') {
-            $src = substr($src, 3);
-        }
+		if (TYPO3_MODE === 'BE' && substr($src, 0, 3) === '../') {
+			$src = substr($src, 3);
+		}
 
-        $this->imageInfo = $this->contentObject->getImgResource($src, $setup);
+		$this->imageInfo = $this->contentObject->getImgResource($src, $setup);
 
-        $GLOBALS['TSFE']->lastImageInfo = $this->imageInfo;
+		$GLOBALS['TSFE']->lastImageInfo = $this->imageInfo;
 
-        if (!is_array($this->imageInfo)) {
-            throw new Tx_Fluid_Core_ViewHelper_Exception('Could not get image resource for "' . htmlspecialchars($src) . '".' , 1253191060);
-        }
+		if (!is_array($this->imageInfo)) {
+			throw new Tx_Fluid_Core_ViewHelper_Exception('Could not get image resource for "' . htmlspecialchars($src) . '".' , 1253191060);
+		}
 
-        $this->imageInfo[3] = t3lib_div::png_to_gif_by_imagemagick($this->imageInfo[3]);
+		$this->imageInfo[3] = t3lib_div::png_to_gif_by_imagemagick($this->imageInfo[3]);
 
-        $GLOBALS['TSFE']->imagesOnPage[] = $this->imageInfo[3];
+		$GLOBALS['TSFE']->imagesOnPage[] = $this->imageInfo[3];
 
-        $this->mediaSource = $GLOBALS['TSFE']->absRefPrefix . t3lib_div::rawUrlEncodeFP($this->imageInfo[3]);
+		$this->mediaSource = $GLOBALS['TSFE']->absRefPrefix . t3lib_div::rawUrlEncodeFP($this->imageInfo[3]);
 
-        if (TYPO3_MODE === 'BE') {
-            $this->resetFrontendEnvironment();
-        }
-    }
+		if (TYPO3_MODE === 'BE') {
+			$this->resetFrontendEnvironment();
+		}
+	}
 
-    /**
-     * Prepares $GLOBALS['TSFE'] for Backend mode
-     * This somewhat hacky work around is currently needed because the getImgResource() function of tslib_cObj relies on those variables to be set
-     *
-     * @return void
-     */
-    protected function simulateFrontendEnvironment() {
-        $this->tsfeBackup = isset($GLOBALS['TSFE']) ? $GLOBALS['TSFE'] : NULL;
-            // set the working directory to the site root
-        $this->workingDirectoryBackup = getcwd();
-        chdir(PATH_site);
+	/**
+	 * Prepares $GLOBALS['TSFE'] for Backend mode
+	 * This somewhat hacky work around is currently needed because the getImgResource() function of tslib_cObj relies on those variables to be set
+	 *
+	 * @return void
+	 */
+	protected function simulateFrontendEnvironment() {
+		$this->tsfeBackup = isset($GLOBALS['TSFE']) ? $GLOBALS['TSFE'] : NULL;
+			// set the working directory to the site root
+		$this->workingDirectoryBackup = getcwd();
+		chdir(PATH_site);
 
-        $typoScriptSetup = $this->configurationManager->getConfiguration(Tx_Extbase_Configuration_ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
-        $GLOBALS['TSFE'] = new stdClass();
-        $template = t3lib_div::makeInstance('t3lib_TStemplate');
-        $template->tt_track = 0;
-        $template->init();
-        $template->getFileName_backPath = PATH_site;
-        $GLOBALS['TSFE']->tmpl = $template;
-        $GLOBALS['TSFE']->tmpl->setup = $typoScriptSetup;
-        $GLOBALS['TSFE']->config = $typoScriptSetup;
-    }
+		$typoScriptSetup = $this->configurationManager->getConfiguration(Tx_Extbase_Configuration_ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
+		$GLOBALS['TSFE'] = new stdClass();
+		$template = t3lib_div::makeInstance('t3lib_TStemplate');
+		$template->tt_track = 0;
+		$template->init();
+		$template->getFileName_backPath = PATH_site;
+		$GLOBALS['TSFE']->tmpl = $template;
+		$GLOBALS['TSFE']->tmpl->setup = $typoScriptSetup;
+		$GLOBALS['TSFE']->config = $typoScriptSetup;
+	}
 
-    /**
-     * Resets $GLOBALS['TSFE'] if it was previously changed by simulateFrontendEnvironment()
-     *
-     * @return void
-     * @see simulateFrontendEnvironment()
-     */
-    protected function resetFrontendEnvironment() {
-        $GLOBALS['TSFE'] = $this->tsfeBackup;
-        chdir($this->workingDirectoryBackup);
-    }
+	/**
+	 * Resets $GLOBALS['TSFE'] if it was previously changed by simulateFrontendEnvironment()
+	 *
+	 * @return void
+	 * @see simulateFrontendEnvironment()
+	 */
+	protected function resetFrontendEnvironment() {
+		$GLOBALS['TSFE'] = $this->tsfeBackup;
+		chdir($this->workingDirectoryBackup);
+	}
 
 }
