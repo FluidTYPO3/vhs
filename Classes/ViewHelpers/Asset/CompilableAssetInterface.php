0370d39529a0d9e5197dff916302478c058e74e1
Claus Due <claus@wildside.dk>
2013-02-10 00:55:20 +0100

    [FEATURE] Interfaces for CompilableAsset and AssetCompiler

---
+++ Classes/ViewHelpers/Asset/CompilableAssetInterface.php
@@ -0,0 +1,44 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Claus Due <claus@wildside.dk>, Wildside A/S
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ * ************************************************************* */
+
+/**
+ * Basic interface for compilable Assets, which are a sub
+ * class of Assets that do not get included immediate but
+ * rather are collected and processed in bulk by an
+ * associated AssetCompilerInterface implementation. The
+ * association between CompilableAsset
+ *
+ * @author Claus Due <claus@wildside.dk>, Wildside A/S
+ * @package Vhs
+ * @subpackage ViewHelpers\Asset
+ */
+interface Tx_Vhs_ViewHelpers_Asset_CompilableAssetInterface extends Tx_Vhs_ViewHelpers_Asset_AssetInterface {
+
+	/**
+	 * @return string
+	 */
+	public function getCompilerClassName();
+
+}
