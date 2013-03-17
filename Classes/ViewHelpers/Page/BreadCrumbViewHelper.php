f713418adea33ee568ebb9c778747d4423256197
Claus Due <claus@wildside.dk>
2013-03-17 13:32:14 +0100

    [TASK] Set variable backup in Menu / BreadCrumb ViewHelper

--- Classes/ViewHelpers/Page/BreadCrumbViewHelper.php
+++ Classes/ViewHelpers/Page/BreadCrumbViewHelper.php
@@ -34,6 +34,11 @@
 class Tx_Vhs_ViewHelpers_Page_BreadCrumbViewHelper extends Tx_Vhs_ViewHelpers_Page_Menu_AbstractMenuViewHelper {
 
 	/**
+	 * @var array
+	 */
+	protected $backups = array('rootLine');
+
+	/**
 	 * @return void
 	 */
 	public function initializeArguments() {
