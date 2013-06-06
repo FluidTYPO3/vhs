c35827cdbba757783534718b004bba55e4e4a8be
Björn Fromme <mail@bjo3rn.com>
2013-06-06 11:29:48 +0200

    [FEATURE] Add isDomainObject ViewHelper

---
+++ Classes/ViewHelpers/If/Type/IsDomainObjectViewHelper.php
@@ -0,0 +1,57 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2013 Björn Fromme <fromme@dreipunktnull.com>, dreipunktnull
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * ### Condition: Value is a domain object
+ *
+ * Condition ViewHelper which renders the `then` child if provided
+ * value is a domain object, i.e. it inherits from extbase's base
+ * class
+ *
+ * @author Björn Fromme <fromme@dreipunktnull.com>, dreipunktnull
+ * @package Vhs
+ * @subpackage ViewHelpers\If\Type
+ */
+class Tx_Vhs_ViewHelpers_If_Type_IsDomainObjectViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractConditionViewHelper {
+
+	/**
+	 * Render method
+	 *
+	 * @param mixed $value
+	 * @return string
+	 */
+	public function render($value) {
+		if ('4' === substr(TYPO3_version, 0, 1)) {
+			$baseClass = 'Tx_Extbase_DomainObject_AbstractDomainObject';
+		} else {
+			$baseClass = 'TYPO3\\CMS\\Extbase\\DomainObject\\AbstractDomainObject';
+		}
+		if (TRUE === ($value instanceof $baseClass)) {
+			return $this->renderThenChild();
+		} else {
+			return $this->renderElseChild();
+		}
+	}
+}
