1c6992149695c33ec58d992ca0bef89c6ad1a926
Claus Due <claus@wildside.dk>
2013-07-04 20:49:30 +0200

    [FEATURE] Option to exclude display of untranslated content

--- Classes/ViewHelpers/Content/AbstractContentViewHelper.php
+++ Classes/ViewHelpers/Content/AbstractContentViewHelper.php
@@ -82,6 +82,7 @@ abstract class Tx_Vhs_ViewHelpers_Content_AbstractContentViewHelper extends Tx_F
 		$this->registerArgument('slideCollectReverse', 'boolean', 'Normally when collecting content elements the elements from the actual page get shown on the top and those from the parent pages below those. You can invert this behaviour (actual page elements at bottom) by setting this flag))', FALSE, 0);
 		$this->registerArgument('loadRegister', 'array', 'List of LOAD_REGISTER variable');
 		$this->registerArgument('render', 'boolean', 'Optional returning variable as original table rows', FALSE, TRUE);
+		$this->registerArgument('hideUnTranslated', 'boolean', 'If FALSE, will NOT include elements which have NOT been translated, if current language is NOT the default language. Default is to show untranslated elements but never display the original if there is a translated version', FALSE, FALSE);
 	}
 
 	/**
@@ -131,6 +132,13 @@ abstract class Tx_Vhs_ViewHelpers_Content_AbstractContentViewHelper extends Tx_F
 		}
 
 		$content = array();
+		$hideUnTranslated = (boolean) $this->arguments['hideUnTranslated'];
+		$currentLanguage = $GLOBALS['TSFE']->sys_language_uid;
+		$languageCondition = '(sys_language_uid IN (-1,' . $currentLanguage . ')';
+		if ($hideUnTranslated && 0 < $currentLanguage) {
+			$languageCondition .= ' AND l18n_parent > 0';
+		}
+		$languageCondition .= ')';
 		do {
 			if ($slide) {
 				$page = array_shift($rootLine);
@@ -144,8 +152,7 @@ abstract class Tx_Vhs_ViewHelpers_Content_AbstractContentViewHelper extends Tx_F
 			} else {
 				$conditions = "pid = '" . $pid ."' AND colPos = '" . $colPos . "'" .
 					$GLOBALS['TSFE']->cObj->enableFields('tt_content') .
-					" AND (sys_language_uid IN (-1,0) OR (sys_language_uid = '" .
-					$GLOBALS['TSFE']->sys_language_uid . "' AND l18n_parent = '0'))";
+					' AND ' . $languageCondition;
 			}
 			$rows = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows('*', 'tt_content', $conditions, 'uid', $order, $limit);
 			$content = (FALSE === (boolean) $this->arguments['render']) ? $rows : $this->getRenderedRecord($rows);
