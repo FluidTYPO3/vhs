4d3be347ee7fa03061baa5316bbf43f0dcbdcf4d
Benjamin Rau <rau@codearts.at>
2013-09-25 15:14:40 +0200

    [FEATURE] Chunk ViewHelper
    array_chunk() is a really helpful function which functionality should be available as a ViewHelper.
    
    Fx if you have an Array of pages from v:page.menu and want to allocate these links to a fixed number of container elements this would be much code without this ViewHelper.
    
    Fx we have 9 links which shall be placed in 2 containers of same count of items each, if odd the first container takes more all.
    
    Expected html markup:
    <div class="container1">
    <a>1. Link</a>
    <a>2. Link</a>
    <a>3. Link</a>
    <a>4. Link</a>
    <a>5. Link</a>
    </div>
    <div class="container2">
    <a>6. Link</a>
    <a>7. Link</a>
    <a>8. Link</a>
    <a>9. Link</a>
    </div>
    
    With this ViewHelper this is an easy task:
    {links->v:iterator.chunk(count:2,fixed:1)->v:var.set(name:'chunks')}
    <div class="container1">
    <f:for each="{chunks.0}" as="item">{item}</f:for>
    </div>
    <div class="container2">
    <f:for each="{chunks.1}" as="item">{item}</f:for>
    </div>

---
+++ Classes/ViewHelpers/Iterator/ChunkViewHelper.php
@@ -0,0 +1,77 @@
+<?php
+/***************************************************************
+ *  Copyright notice
+ *
+ *  (c) 2012 Claus Due <claus@wildside.dk>, Wildside A/S
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
+
+/**
+ * Creates chunks from an input Array/Traversable with option to allocate items to a fixed number of chunks
+ *
+ * @author Benjamin Rau <rau@codearts.at>, codearts
+ * @package Vhs
+ * @subpackage ViewHelpers\Iterator
+ */
+class Tx_Vhs_ViewHelpers_Iterator_ChunkViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractViewHelper
+{
+
+	/**
+	 * Render method
+	 *
+	 * @param int $count The count of items per chunk or if fixed number of chunks
+	 * @param boolean $fixed Whether to allocate items to a fixed number of chunks or not
+	 * @param mixed $subject The subject Traversable/Array instance to shift
+	 * @param string $as If specified, inserts a template variable with this name, then renders the child content, then removes the variable
+	 * @throws Exception
+	 * @return array
+	 */
+	public function render($count, $fixed = FALSE, $subject = NULL, $as = NULL) {
+		if (NULL === $subject) {
+			$subject = $this->renderChildren();
+		}
+		if (TRUE === $subject instanceof Traversable) {
+			$subject = iterator_to_array($subject, TRUE);
+		} elseif (TRUE !== is_array($subject)) {
+			throw new Exception('Cannot get values of unsupported type: ' . gettype($subject), 1357098192);
+		}
+		if (TRUE === $fixed) {
+			$subjectSize = count($subject);
+			$chunkSize = ceil($subjectSize / $count);
+			$output = array_chunk($subject, $chunkSize);
+		} else {
+			$output = array_chunk($subject, $count);
+		}
+		if (NULL !== $as) {
+			if ($this->templateVariableContainer->exists($as)) {
+				$backup = $this->templateVariableContainer->get($as);
+				$this->templateVariableContainer->remove($as);
+			}
+			$this->templateVariableContainer->add($as, $output);
+			$output = $this->renderChildren();
+			$this->templateVariableContainer->remove($as);
+			if (TRUE === isset($backup)) {
+				$this->templateVariableContainer->add($as, $backup);
+			}
+		}
+		return $output;
+	}
+
+}
