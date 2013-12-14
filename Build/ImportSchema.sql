2f33a5658d3b7dd01ef7448fbbeea7211d3a70c0
Claus Due <claus@namelesscoder.net>
2013-12-14 22:32:55 +0100

    [TASK] Upgrade Travis build
    Happy now, Travis?

---
+++ Build/ImportSchema.sql
@@ -0,0 +1,1 @@
+INSERT INTO be_users (pid, tstamp, username, password, admin, usergroup, disable, starttime, endtime, lang, email) VALUES (0,1276860841,'_cli_lowlevel','5f4dcc3b5aa765d61d8327deb882cf99',0,'1',0,0,0,'','_cli_phpunit@example.com');
