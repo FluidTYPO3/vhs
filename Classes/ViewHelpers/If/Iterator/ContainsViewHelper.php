f9faca970f0d537242651764bdf44734abafa187
Björn Fromme <mail@bjo3rn.com>
2013-12-16 17:51:09 +0100

    [TASK] Refactor class inheritance

--- Classes/ViewHelpers/If/Iterator/ContainsViewHelper.php
+++ Classes/ViewHelpers/If/Iterator/ContainsViewHelper.php
@@ -31,7 +31,7 @@
  * @package Vhs
  * @subpackage ViewHelpers\If\Iterator
  */
-class Tx_Vhs_ViewHelpers_If_Iterator_ContainsViewHelper extends Tx_Vhs_ViewHelpers_Iterator_AbstractIteratorViewHelper {
+class Tx_Vhs_ViewHelpers_If_Iterator_ContainsViewHelper extends Tx_Fluid_Core_ViewHelper_AbstractConditionViewHelper {
 
 	/**
 	 * @var mixed
@@ -42,7 +42,6 @@ class Tx_Vhs_ViewHelpers_If_Iterator_ContainsViewHelper extends Tx_Vhs_ViewHelpe
 	 * Initialize arguments
 	 */
 	public function initializeArguments() {
-		parent::initializeArguments();
 		$this->registerArgument('needle', 'mixed', 'Needle to search for in haystack', TRUE);
 		$this->registerArgument('haystack', 'mixed', 'Haystack in which to look for needle', TRUE);
 		$this->registerArgument('considerKeys', 'boolean', 'Tell whether to consider keys in the search assuming haystack is an array.', FALSE, FALSE);
@@ -66,5 +65,124 @@ class Tx_Vhs_ViewHelpers_If_Iterator_ContainsViewHelper extends Tx_Vhs_ViewHelpe
 		}
 	}
 
+	/**
+	 * @param integer $index
+	 * @return mixed
+	 */
+	protected function getNeedleAtIndex($index) {
+		if ($index < 0) {
+			return NULL;
+		}
+		$haystack = $this->arguments['haystack'];
+		$asArray = array();
+		if (is_array($haystack)) {
+			$asArray = $haystack;
+		} elseif ($haystack instanceof Tx_Extbase_Persistence_ObjectStorage) {
+			/** @var $haystack Tx_Extbase_Persistence_ObjectStorage */
+			$asArray = $haystack->toArray();
+		} elseif ($haystack instanceof Tx_Extbase_Persistence_LazyObjectStorage) {
+			/** @var $haystack Tx_Extbase_Persistence_LazyObjectStorage */
+			$asArray = $haystack->toArray();
+		} elseif ($haystack instanceof Tx_Extbase_Persistence_QueryResult) {
+			/** @var $haystack Tx_Extbase_Persistence_QueryResult */
+			$asArray = $haystack->toArray();
+		} elseif (is_string($haystack)) {
+			$asArray = str_split($haystack);
+		}
+		return isset($asArray[$index]) ? $asArray[$index] : FALSE;
+	}
+
+	/**
+	 * @param mixed $haystack
+	 * @param mixed $needle
+	 * @return boolean
+	 */
+	protected function assertHaystackHasNeedle($haystack, $needle) {
+		if (is_array($haystack)) {
+			return FALSE !== $this->assertHaystackIsArrayAndHasNeedle($haystack, $needle);
+		} elseif ($haystack instanceof Tx_Extbase_Persistence_ObjectStorage) {
+			return FALSE !== $this->assertHaystackIsObjectStorageAndHasNeedle($haystack, $needle);
+		} elseif ($haystack instanceof Tx_Extbase_Persistence_LazyObjectStorage) {
+			return FALSE !== $this->assertHaystackIsObjectStorageAndHasNeedle($haystack, $needle);
+		} elseif ($haystack instanceof Tx_Extbase_Persistence_QueryResult) {
+			return FALSE !== $this->assertHaystackIsQueryResultAndHasNeedle($haystack, $needle);
+		} elseif (is_string($haystack)) {
+			return FALSE !== strpos($haystack, $needle);
+		}
+		return FALSE;
+	}
+
+	/**
+	 * @param mixed $haystack
+	 * @param mixed $needle
+	 * @return mixed
+	 */
+	protected function assertHaystackIsQueryResultAndHasNeedle($haystack, $needle) {
+		if ($needle instanceof Tx_Extbase_DomainObject_DomainObjectInterface) {
+			/** @var $needle Tx_Extbase_DomainObject_DomainObjectInterface */
+			$needle = $needle->getUid();
+		}
+		foreach ($haystack as $index => $candidate) {
+			/** @var $candidate Tx_Extbase_DomainObject_DomainObjectInterface */
+			if ($candidate->getUid() == $needle) {
+				return $index;
+			}
+		}
+		return FALSE;
+	}
+
+	/**
+	 * @param mixed $haystack
+	 * @param mixed $needle
+	 * @return mixed
+	 */
+	protected function assertHaystackIsObjectStorageAndHasNeedle($haystack, $needle) {
+		$index = 0;
+		/** @var $candidate Tx_Extbase_DomainObject_DomainObjectInterface */
+		if ($needle instanceof Tx_Extbase_DomainObject_AbstractDomainObject) {
+			$needle = $needle->getUid();
+		}
+		foreach ($haystack as $candidate) {
+			if ($candidate->getUid() == $needle) {
+				return $index;
+			}
+			$index++;
+		}
+		return FALSE;
+	}
+
+	/**
+	 * @param mixed $haystack
+	 * @param mixed $needle
+	 * @return boolean
+	 */
+	protected function assertHaystackIsArrayAndHasNeedle($haystack, $needle) {
+		if ($needle instanceof Tx_Extbase_DomainObject_DomainObjectInterface === FALSE) {
+			if (isset($this->arguments['considerKeys']) && $this->arguments['considerKeys']) {
+				$result = array_search($needle, $haystack) || isset($haystack[$needle]);
+			} else {
+				$result = array_search($needle, $haystack);
+			}
+			return $result;
+		} else {
+			/** @var $needle Tx_Extbase_DomainObject_DomainObjectInterface */
+			foreach ($haystack as $index => $straw) {
+				/** @var $straw Tx_Extbase_DomainObject_DomainObjectInterface */
+				if ($straw->getUid() == $needle->getUid()) {
+					return $index;
+				}
+			}
+		}
+		return FALSE;
+	}
+
+	/**
+	 * @param mixed $haystack
+	 * @param mixed $needle
+	 * @return boolean
+	 */
+	protected function assertHaystackIsStringAndHasNeedle($haystack, $needle) {
+		return strpos($haystack, $needle);
+	}
 
 }
--- Classes/ViewHelpers/Iterator/NextViewHelper.php
+++ Classes/ViewHelpers/Iterator/NextViewHelper.php
@@ -30,7 +30,7 @@
  * @package Vhs
  * @subpackage ViewHelpers\Iterator
  */
-class Tx_Vhs_ViewHelpers_Iterator_NextViewHelper extends Tx_Vhs_ViewHelpers_Iterator_ContainsViewHelper {
+class Tx_Vhs_ViewHelpers_Iterator_NextViewHelper extends Tx_Vhs_ViewHelpers_If_Iterator_ContainsViewHelper {
 
 	/**
 	 * Render method
