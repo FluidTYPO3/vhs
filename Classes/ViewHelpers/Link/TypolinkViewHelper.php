eb94d0c890d4ef56c939e7e6abfa85462f5b713a
Cedric Ziel <cedric@cedric-ziel.com>
2014-01-09 14:19:09 +0100

    [DOC] Enhance TypoLink ViewHelper Documentation
    Made it clear how a simple link could look like and referenced core docs

--- Classes/ViewHelpers/Link/TypolinkViewHelper.php
+++ Classes/ViewHelpers/Link/TypolinkViewHelper.php
@@ -28,11 +28,22 @@
  * Renders a link with the TypoLink function.
  * Can be used with the LinkWizard
  *
+ * For more info on the typolink function, please consult the offical core-documentation:
+ * http://docs.typo3.org/typo3cms/TyposcriptIn45MinutesTutorial/TypoScriptFunctions/Typolink/Index.html
+ *
  * ### Examples
  *
  *     <!-- tag -->
  *     <v:link.typolink configuration="{typoLinkConfiguration}" />
  *     <v:link.typolink configuration="{object}">My LinkText</v:link.typolink>
+ *     <!-- with a {parameter} variable containing the PID -->
+ *     <v:link.typolink configuration="{parameter: parameter}" />
+ *     <!-- with a {fields.link} variable from the LinkWizard (incl. 'class', 'target' etc.) inside a flux form -->
+ *     <v:link.typolink configuration="{parameter: fields.link}" />
+ *     <!-- same with a {page} variable from fluidpages -->
+ *     <v:link.typolink configuration="{parameter: page.uid}" />
+ *     <!-- With extensive configuration -->
+ *     <v:link.typolink configuration="{parameter: page.uid, additionalParams: '&print=1', title: 'Follow the link'}">Click Me!</v:link.typolink>
  *
  * @author Cedric Ziel <cedric@cedric-ziel.com>, Cedric Ziel - Internetdienstleistungen & EDV
  * @package Vhs
--- Classes/ViewHelpers/Uri/TypolinkViewHelper.php
+++ Classes/ViewHelpers/Uri/TypolinkViewHelper.php
@@ -28,12 +28,22 @@
  * Renders a uri with the TypoLink function.
  * Can be used with the LinkWizard
  *
+ * For more info on the typolink function, please consult the offical core-documentation:
+ * http://docs.typo3.org/typo3cms/TyposcriptIn45MinutesTutorial/TypoScriptFunctions/Typolink/Index.html
+ *
  * ### Examples
  *
  *     <!-- tag -->
  *     <v:uri.typolink configuration="{typoLinkConfiguration}" />
- *     <!-- inline -->
- *     {v:uri.typolink( configuration: '{typoLinkConfiguration}'}
+ *     <v:uri.typolink configuration="{object}">My LinkText</v:uri.typolink>
+ *     <!-- with a {parameter} variable containing the PID -->
+ *     <v:uri.typolink configuration="{parameter: parameter}" />
+ *     <!-- with a {fields.link} variable from the LinkWizard (incl. 'class', 'target' etc.) inside a flux form -->
+ *     <v:uri.typolink configuration="{parameter: fields.link}" />
+ *     <!-- same with a {page} variable from fluidpages -->
+ *     <v:uri.typolink configuration="{parameter: page.uid}" />
+ *     <!-- With extensive configuration -->
+ *     <v:uri.typolink configuration="{parameter: page.uid, additionalParams: '&print=1', title: 'Follow the link'}">Click Me!</v:uri.typolink>
  *
  * @author Cedric Ziel <cedric@cedric-ziel.com>, Cedric Ziel - Internetdienstleistungen & EDV
  * @package Vhs
