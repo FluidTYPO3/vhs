db6d54f6cc2dd6885e0e75c43905594cf61f8718
Frank Gerards <frank@f03.eu>
2014-02-20 16:58:06 +0100

    [FEATURE] Add attribute "format" for target file format

--- Classes/ViewHelpers/Media/Image/AbstractImageViewHelper.php
+++ Classes/ViewHelpers/Media/Image/AbstractImageViewHelper.php
@@ -78,6 +78,7 @@ abstract class Tx_Vhs_ViewHelpers_Media_Image_AbstractImageViewHelper extends Tx
 		parent::initializeArguments();
 		$this->registerArgument('width', 'string', 'Width of the image. This can be a numeric value representing the fixed width of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.', FALSE);
 		$this->registerArgument('height', 'string', 'Height of the image. This can be a numeric value representing the fixed height of the image in pixels. But you can also perform simple calculations by adding "m" or "c" to the value. See imgResource.width for possible options.', FALSE);
+		$this->registerArgument('format', 'string', 'Format of the processed file - also determines the target file format. If blank, TYPO3/IM/GM default is taken into account.', FALSE, NULL);
 		$this->registerArgument('treatIdAsReference', 'boolean', 'When TRUE treat given src argument as sys_file_reference record. Applies only to TYPO3 6.x and above.', FALSE, FALSE);
 	}
 
@@ -93,6 +94,7 @@ abstract class Tx_Vhs_ViewHelpers_Media_Image_AbstractImageViewHelper extends Tx
 		$minH = $this->arguments['minH'];
 		$maxW = $this->arguments['maxW'];
 		$maxH = $this->arguments['maxH'];
+		$format = $this->arguments['format'];
 		$treatIdAsReference = (boolean) $this->arguments['treatIdAsReference'];
 
 		if (TYPO3_MODE === 'BE') {
@@ -107,6 +109,9 @@ abstract class Tx_Vhs_ViewHelpers_Media_Image_AbstractImageViewHelper extends Tx
 			'maxH' => $maxH,
 			'treatIdAsReference' => $treatIdAsReference,
 		);
+		if (FALSE === empty($format)) {
+			$setup['ext'] = $format;
+		}
 		if (TYPO3_MODE === 'BE' && substr($src, 0, 3) === '../') {
 			$src = substr($src, 3);
 		}
