39435e241f56d1021d85a6b080c80164dfa6fd6f
Claus Due <claus@namelesscoder.net>
2014-06-18 16:58:24 +0200

    [TASK] Update hook definitions to new namespace classes

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,9 +3,9 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = 'Tx_Vhs_Service_AssetService->buildAll';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['hook_eofe'][] = 'Tx_Vhs_Service_AssetService->buildAllUncached';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] = 'Tx_Vhs_Service_AssetService->clearCacheCommand';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] = '\FluidTYPO3\Vhs\Service\AssetService->buildAll';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['hook_eofe'][] = '\FluidTYPO3\Vhs\Service\AssetService->buildAllUncached';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] = '\FluidTYPO3\Vhs\Service\AssetService->clearCacheCommand';
 
 
 if (FALSE === is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations']['vhs_main'])) {
