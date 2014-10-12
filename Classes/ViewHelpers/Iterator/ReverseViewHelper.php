983329ce2ebf6b2d15ac1371e791bb4663b53efa
Claus Due <claus@namelesscoder.net>
2014-10-12 21:02:23 +0200

    [BUGFIX] Iterator / Reverse support for unfired QueryResult

--- Classes/ViewHelpers/Iterator/ReverseViewHelper.php
+++ Classes/ViewHelpers/Iterator/ReverseViewHelper.php
@@ -74,18 +74,18 @@ class ReverseViewHelper extends AbstractViewHelper {
 		if (TRUE === is_array($subject)) {
 			$array = $subject;
 		} else {
-			if (TRUE === $subject instanceof \Iterator) {
-				/** @var Iterator $subject */
-				$array = iterator_to_array($subject, TRUE);
-			} elseif (TRUE === $subject instanceof QueryResultInterface) {
+			if (TRUE === $subject instanceof QueryResultInterface) {
 				/** @var QueryResultInterface $subject */
 				$array = $subject->toArray();
+			} elseif (TRUE === $subject instanceof \Traversable) {
+				/** @var Iterator $subject */
+				$array = iterator_to_array($subject, TRUE);
 			} elseif (NULL !== $subject) {
 				// a NULL value is respected and ignored, but any
 				// unrecognized value other than this is considered a
 				// fatal error.
 				throw new Exception('Invalid variable type passed to Iterator/ReverseViewHelper. Expected any of Array, QueryResult, ' .
-					' ObjectStorage or Iterator implementation but got ' . gettype($subject), 1351958941);
+					'ObjectStorage or Iterator implementation but got ' . gettype($subject), 1351958941);
 			}
 		}
 		$array = array_reverse($array, TRUE);
--- Tests/Unit/ViewHelpers/Iterator/ReverseViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Iterator/ReverseViewHelperTest.php
@@ -33,4 +33,63 @@ use FluidTYPO3\Vhs\ViewHelpers\AbstractViewHelperTest;
  */
 class ReverseViewHelperTest extends AbstractViewHelperTest {
 
+	/**
+	 * @test
+	 * @dataProvider getRenderTestValues
+	 * @param array $arguments
+	 * @param mixed $expectedValue
+	 */
+	public function testRender(array $arguments, $expectedValue) {
+		if (TRUE === isset($arguments['as'])) {
+			$value = $this->executeViewHelperUsingTagContent('ObjectAccessor', 'variable', $arguments);
+		} else {
+			$value = $this->executeViewHelper($arguments);
+			$value2 = $this->executeViewHelperUsingTagContent('ObjectAccessor', 'v', array(), array('v' => $arguments['subject']));
+			$this->assertEquals($value, $value2);
+		}
+		$this->assertEquals($value, $expectedValue);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getRenderTestValues() {
+		$queryResult = $this->getMock('TYPO3\\CMS\\Extbase\\Persistence\\Generic\\QueryResult',
+			array('toArray', 'initialize', 'rewind', 'valid'), array(), '', FALSE);
+		$queryResult->expects($this->any())->method('toArray')->will($this->returnValue(array('foo', 'bar')));
+		$queryResult->expects($this->any())->method('valid')->will($this->returnValue(FALSE));
+		return array(
+			array(array('subject' => array()), array()),
+			array(array('subject' => array('foo', 'bar')), array(1 => 'bar', 0 => 'foo')),
+			array(array('subject' => array('foo', 'bar'), 'as' => 'variable'), array(1 => 'bar', 0 => 'foo')),
+			array(array('subject' => new \ArrayIterator(array('foo', 'bar'))), array(1 => 'bar', 0 => 'foo')),
+			array(array('subject' => new \ArrayIterator(array('foo', 'bar')), 'as' => 'variable'), array(1 => 'bar', 0 => 'foo')),
+			array(array('subject' => $queryResult), array(1 => 'bar', 0 => 'foo')),
+			array(array('subject' => $queryResult, 'as' => 'variable'), array(1 => 'bar', 0 => 'foo'))
+		);
+	}
+
+	/**
+	 * @test
+	 * @dataProvider getErrorTestValues
+	 * @param mixed $subject
+	 */
+	public function testThrowsErrorsOnInvalidSubjectType($subject) {
+		$expected = 'Invalid variable type passed to Iterator/ReverseViewHelper. Expected any of Array, QueryResult, ' .
+			'ObjectStorage or Iterator implementation but got ' . gettype($subject);
+		$result = $this->executeViewHelper(array('subject' => $subject));
+		$this->assertEquals($expected, $result, $result);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function getErrorTestValues() {
+		return array(
+			array(new \DateTime()),
+			array('invalid'),
+			array(new \stdClass()),
+		);
+	}
+
 }
