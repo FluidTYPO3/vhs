224153da775f0d125747e5ac54ac790eb51ea886
Claus Due <claus@namelesscoder.net>
2014-11-16 02:11:46 +0100

    [TER] 2.1.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,50 +1,42 @@
 <?php
-
-/***************************************************************
- * Extension Manager/Repository config file for ext "vhs".
- *
- * Auto generated 19-09-2014 13:56
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array(
-	'title' => 'VHS: Fluid ViewHelpers',
-	'description' => 'A collection of ViewHelpers to perform rendering tasks which are not natively supported by Fluid - for example: advanced formatters, math calculators, specialized conditions and Iterator/Array calculators and processors',
-	'category' => 'misc',
-	'author' => 'FluidTYPO3 Team',
-	'author_email' => 'claus@namelesscoder.net',
-	'author_company' => '',
-	'shy' => '',
-	'dependencies' => 'cms,extbase,fluid',
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'stable',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'version' => '2.1.3',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.0.0-6.2.99',
-			'cms' => '',
-			'extbase' => '',
-			'fluid' => '',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'suggests' => array(
-	),
-	'_md5_values_when_last_written' => '',
+$EM_CONF[$_EXTKEY] = array (
+  'title' => 'VHS: Fluid ViewHelpers',
+  'description' => 'A collection of ViewHelpers to perform rendering tasks which are not natively supported by Fluid - for example: advanced formatters, math calculators, specialized conditions and Iterator/Array calculators and processors',
+  'category' => 'misc',
+  'author' => 'FluidTYPO3 Team',
+  'author_email' => 'claus@namelesscoder.net',
+  'author_company' => '',
+  'shy' => '',
+  'dependencies' => 'cms,extbase,fluid',
+  'conflicts' => '',
+  'priority' => '',
+  'module' => '',
+  'state' => 'stable',
+  'internal' => '',
+  'uploadfolder' => 0,
+  'createDirs' => '',
+  'modify_tables' => '',
+  'clearCacheOnLoad' => 0,
+  'lockType' => '',
+  'version' => '2.1.4',
+  'constraints' => 
+  array (
+    'depends' => 
+    array (
+      'typo3' => '6.0.0-6.2.99',
+      'cms' => '',
+      'extbase' => '',
+      'fluid' => '',
+    ),
+    'conflicts' => 
+    array (
+    ),
+    'suggests' => 
+    array (
+    ),
+  ),
+  'suggests' => 
+  array (
+  ),
+  '_md5_values_when_last_written' => '',
 );
-
-?>
