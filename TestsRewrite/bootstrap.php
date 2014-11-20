108bc44e0068ffe3c1fa9c05dc9be2629039216f
Cedric Ziel <cedric@cedric-ziel.com>
2014-11-20 15:16:17 +0100

    [TASK] Move phpunit.xml to top

--- TestsRewrite/bootstrap.php
+++ TestsRewrite/bootstrap.php
@@ -5,16 +5,13 @@ if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
 		'Could not find vendor/autoload.php, make sure you ran composer.'
 	);
 }
-
 require_once __DIR__ . '/../vendor/autoload.php';
-
 define('PATH_thisScript', realpath('vendor/typo3/cms/typo3/index.php'));
 define('TYPO3_MODE', 'BE');
-putenv('TYPO3_CONTEXT=Development');
-
+putenv('TYPO3_CONTEXT=Testing');
 \TYPO3\CMS\Core\Core\Bootstrap::getInstance()
 	->baseSetup('typo3/')
-	->loadConfigurationAndInitialize(FALSE)
-	->loadTypo3LoadedExtAndExtLocalconf(FALSE)
-	->initializeLanguageObject()
+	->disableCoreAndClassesCache()
+	//->loadConfigurationAndInitialize(FALSE)
+	//->loadTypo3LoadedExtAndExtLocalconf(FALSE)
 ;
