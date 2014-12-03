4fb8919f7a18d6cb35ad4f568e1c88f9f245242b
Claus Due <claus@namelesscoder.net>
2014-12-03 11:59:40 +0100

    [TER] 2.2.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,12 +18,12 @@ $EM_CONF[$_EXTKEY] = array (
   'modify_tables' => '',
   'clearCacheOnLoad' => 0,
   'lockType' => '',
-  'version' => '2.1.4',
+  'version' => '2.2.0',
   'constraints' =>
   array (
     'depends' =>
     array (
-      'typo3' => '6.0.0-6.2.99',
+      'typo3' => '6.0.0-7.99.99',
       'cms' => '',
       'extbase' => '',
       'fluid' => '',
