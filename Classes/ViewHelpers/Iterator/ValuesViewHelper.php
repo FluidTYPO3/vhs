3a1a2554369b43fe74041808db9c3d8ba0172bc0
Claus Due <claus@namelesscoder.net>
2015-01-20 04:38:56 +0100

    [TASK] Convert an Exception to ViewHelper Exception
    Conflicts:
    	Classes/ViewHelpers/Iterator/ValuesViewHelper.php

--- Classes/ViewHelpers/Iterator/ValuesViewHelper.php
+++ Classes/ViewHelpers/Iterator/ValuesViewHelper.php
@@ -1,15 +1,33 @@
 <?php
 namespace FluidTYPO3\Vhs\ViewHelpers\Iterator;
 
-/*
- * This file is part of the FluidTYPO3/Vhs project under GPLv2 or later.
+/***************************************************************
+ *  Copyright notice
  *
- * For the full copyright and license information, please read the
- * LICENSE.md file that was distributed with this source code.
- */
+ *  (c) 2014 Claus Due <claus@namelesscoder.net>
+ *
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 use FluidTYPO3\Vhs\Utility\ViewHelperUtility;
 use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Fluid\Core\ViewHelper\Exception;
 
 /**
  * Gets values from an iterator, removing current keys (if any exist)
@@ -35,7 +53,7 @@ class ValuesViewHelper extends AbstractViewHelper {
 		if (TRUE === $subject instanceof \Iterator) {
 			$subject = iterator_to_array($subject, TRUE);
 		} elseif (FALSE === is_array($subject)) {
-			throw new \Exception('Cannot get values of unsupported type: ' . gettype($subject), 1357098192);
+			throw new Exception('Cannot get values of unsupported type: ' . gettype($subject), 1357098192);
 		}
 		$output = array_values($subject);
 		if (NULL !== $as) {
