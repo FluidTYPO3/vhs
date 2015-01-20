83483f95442f337f05180d09ecacda01e7ddec8b
Claus Due <claus@namelesscoder.net>
2015-01-20 04:39:26 +0100

    [TASK] Add a few missing unit tests

--- Tests/Unit/ViewHelpers/Iterator/ValuesViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Iterator/ValuesViewHelperTest.php
@@ -17,4 +17,44 @@ use FluidTYPO3\Vhs\Tests\Unit\ViewHelpers\AbstractViewHelperTest;
  */
 class ValuesViewHelperTest extends AbstractViewHelperTest {
 
+	/**
+	 * @test
+	 */
+	public function returnsValuesUsingArgument() {
+		$result = $this->executeViewHelper(array('subject' => array('foo' => 'bar')));
+		$this->assertEquals(array('bar'), $result);
+	}
+
+	/**
+	 * @test
+	 */
+	public function returnsValuesUsingTagContent() {
+		$result = $this->executeViewHelperUsingTagContent('ObjectAccessor', 'test', array(), array('test' => array('foo' => 'bar')));
+		$this->assertEquals(array('bar'), $result);
+	}
+
+	/**
+	 * @test
+	 */
+	public function returnsValuesUsingTagContentAndAsArgument() {
+		$result = $this->executeViewHelperUsingTagContent('ObjectAccessor', 'test.0', array('as' => 'test', 'subject' => array('foo' => 'bar')), array());
+		$this->assertEquals('bar', $result);
+	}
+
+	/**
+	 * @test
+	 */
+	public function supportsIterators() {
+		$result = $this->executeViewHelper(array('subject' => new \ArrayIterator(array('foo' => 'bar'))));
+		$this->assertEquals(array('bar'), $result);
+	}
+
+	/**
+	 * @test
+	 */
+	public function returnsErrorOnUnsupportedSubject() {
+		$result = $this->executeViewHelper(array('subject' => new \DateTime()));
+		$this->assertStringStartsWith('Cannot get values of unsupported type', $result);
+	}
+
 }
