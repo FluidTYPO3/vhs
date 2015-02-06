aa3a36f5ef3109c1be0bb6db6f6ff770cd33f2ee
Claus Due <claus@namelesscoder.net>
2015-02-06 13:30:09 +0100

    [TASK] Remove redundant ext_autoload.php file

--- ext_autoload.php
+++
@@ -1,6 +0,0 @@
-<?php
-$extensionPath = t3lib_extMgm::extPath('vhs') . '/';
-
-return array(
-	'FluidTYPO3\\Vhs\\ViewHelpers\\AbstractViewHelperTest' => $extensionPath . 'Tests/Unit/ViewHelpers/AbstractViewHelperTest.php',
-);
