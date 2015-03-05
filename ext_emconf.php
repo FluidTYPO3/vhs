d1b732dbcd61fbdfd27df323265cbcb77618b4a3
Claus Due <claus@namelesscoder.net>
2015-03-05 01:59:50 +0100

    [TASK] Raise PHP requirement to 5.4

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,6 +23,7 @@ $EM_CONF[$_EXTKEY] = array (
   array (
     'depends' =>
     array (
+      'php' => '5.4.0-5.6.99',
       'typo3' => '6.0.0-7.99.99',
       'cms' => '',
       'extbase' => '',
