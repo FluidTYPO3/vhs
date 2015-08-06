4db4ed567f74e3fef25c452fe5d6d337dc79c615
Marc Neuhaus <mneuhaus@famelo.com>
2015-08-06 21:50:10 +0200

    [TASK] Add Responsive Images
    - added a v:media.picture for breakpoint specific Images
    - added srcset argument to v:media.image

--- Classes/ViewHelpers/Media/ImageViewHelper.php
+++ Classes/ViewHelpers/Media/ImageViewHelper.php
@@ -9,17 +9,36 @@ namespace FluidTYPO3\Vhs\ViewHelpers\Media;
  */
 
 use FluidTYPO3\Vhs\ViewHelpers\Media\Image\AbstractImageViewHelper;
+use FluidTYPO3\Vhs\Traits\SourceSetViewHelperTrait;
 
 /**
  * Renders an image tag for the given resource including all valid
  * HTML5 attributes. Derivates of the original image are rendered
  * if the provided (optional) dimensions differ.
  *
+ * ## rendering responsive Images variants
+ *
+ * You can use the srcset argument to generate several differently sized
+ * versions of this image that will be added as a srcset argument to the img tag.
+ * enter a list of widths in the srcset to genereate copies of the same crop +
+ * ratio but in the specified widths.
+ *
+ * ### Example
+ *
+ *     <v:media.image src="fileadmin/some-image.png" srcset="480,768,992,1200" />
+ *
+ * ### Browser Support
+ *
+ * To have the widest Browser-Support you should consider using a polyfill like:
+ * http://scottjehl.github.io/picturefill/
+ *
  * @author Björn Fromme <fromme@dreipunktnull.com>, dreipunktnull
  * @package Vhs
  * @subpackage ViewHelpers\Media
  */
 class ImageViewHelper extends AbstractImageViewHelper {
+
+	use SourceSetViewHelperTrait;
 
 	/**
 	 * name of the tag to be created by this view helper
@@ -41,6 +60,7 @@ class ImageViewHelper extends AbstractImageViewHelper {
 		$this->registerTagAttribute('usemap', 'string', 'A hash-name reference to a map element with which to associate the image.', FALSE);
 		$this->registerTagAttribute('ismap', 'string', 'Specifies that its img element provides access to a server-side image map.', FALSE, '');
 		$this->registerTagAttribute('alt', 'string', 'Equivalent content for those who cannot process images or who have image loading disabled.', TRUE);
+		$this->registerArgument('srcset', 'mixed', 'list of width used for the srcset variants (either CSV, array or implementing Traversable)', FALSE, NULL);
 	}
 
 	/**
@@ -50,8 +70,14 @@ class ImageViewHelper extends AbstractImageViewHelper {
 	 */
 	public function render() {
 		$this->preprocessImage();
-		$src = $this->preprocessSourceUri($this->mediaSource);
-		$this->tag->addAttribute('src', $src);
+
+		if (NULL !== $this->arguments['srcset']) {
+			$this->addSourceSet($this->tag, $this->mediaSource);
+		} else {
+			$src = $this->preprocessSourceUri($this->mediaSource);
+			$this->tag->addAttribute('src', $src);
+		}
+
 		$this->tag->addAttribute('width', $this->imageInfo[0]);
 		$this->tag->addAttribute('height', $this->imageInfo[1]);
 		if ('' === $this->arguments['title']) {
