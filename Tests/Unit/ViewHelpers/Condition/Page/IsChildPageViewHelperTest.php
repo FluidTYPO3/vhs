16e9daddf8b99fa31941c0a6fb8290bdb192c0e9
Marc Neuhaus <mneuhaus@famelo.com>
2015-09-19 23:12:36 +0200

    [TASK] Make Condition/Page ViewHelpers static compilable
    many viewHelpers extending from AbstractConditionViewHelper
    stopped working since 7.3 because the AbstractConditionViewHelper
    is now compiled statically by default. Any ConditionViewHelper that implements
    it's own render method without taking compiling into account currently
    simple fail by showing their "false/else" result as soon as they are
    executed from cache. Non-cached execution still works, which
    makes this problem a bit weird to catch.
    Aside from fixing the ViewHelpers itself the Testcases are updated to verify,
    that the effected viewHelpers render/work the same in cached and uncached
    context.

--- Tests/Unit/ViewHelpers/Condition/Page/IsChildPageViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Condition/Page/IsChildPageViewHelperTest.php
@@ -20,7 +20,16 @@ class IsChildPageViewHelperTest extends AbstractViewHelperTest {
 	public function testRender() {
 		$GLOBALS['TYPO3_DB'] = $this->getMock('TYPO3\\CMS\\Core\\Database\\DatabaseConnection', array('exec_SELECTquery'), array(), '', FALSE);
 		$GLOBALS['TYPO3_DB']->expects($this->any())->method('exec_SELECTquery')->will($this->returnValue(FALSE));
-		$this->assertEquals('else', $this->executeViewHelper(array('then' => 'then', 'else' => 'else', 'pageUid' => 0)));
+		$arguments = array(
+			'then' => 'then',
+			'else' => 'else',
+			'pageUid' => 0
+		);
+		$result = $this->executeViewHelper($arguments);
+		$this->assertEquals('else', $result);
+
+		$staticResult = $this->executeViewHelperStatic($arguments);
+		$this->assertEquals($result, $staticResult, 'The regular viewHelper output doesn\'t match the static output!');
 	}
 
 }
