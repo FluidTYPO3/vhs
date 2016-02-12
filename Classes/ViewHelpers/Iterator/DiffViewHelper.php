dcb6a3738801b8cc122d8e208f02c08bc56a2abf
Thomas Deuling <info@coding.ms>
2016-02-12 12:08:58 +0100

    [FEATURE] Iterator Diff ViewHelper

---
+++ Classes/ViewHelpers/Iterator/DiffViewHelper.php
@@ -0,0 +1,52 @@
+<?php
+namespace FluidTYPO3\Vhs\ViewHelpers\Iterator;
+
+/*
+ * This file is part of the FluidTYPO3/Vhs project under GPLv2 or later.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.md file that was distributed with this source code.
+ */
+
+use FluidTYPO3\Vhs\Traits\ArrayConsumingViewHelperTrait;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+
+/**
+ * Computes the difference of arrays
+ *
+ * @author Thomas Deuling <typo3@coding.ms>, coding.ms
+ * @package Vhs
+ * @subpackage ViewHelpers\Iterator
+ */
+class DiffViewHelper extends AbstractViewHelper {
+
+	use ArrayConsumingViewHelperTrait;
+
+	/**
+	 * Initialize
+	 *
+	 * @return void
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+
+		$this->registerArgument('a', 'mixed', 'First Array/Traversable/CSV', FALSE, NULL);
+		$this->registerArgument('b', 'mixed', 'Second Array/Traversable/CSV', TRUE);
+	}
+
+	/**
+	 * @return array
+	 */
+	public function render() {
+		$a = $this->arguments['a'];
+		if (NULL === $a) {
+			$a = $this->renderChildren();
+		}
+
+		$a = $this->arrayFromArrayOrTraversableOrCSV($a);
+		$b = $this->arrayFromArrayOrTraversableOrCSV($this->arguments['b']);
+
+		return array_diff($a, $b);
+	}
+
+}
