88ae050119ac88e53296b19842cf7e7545e30947
Björn Fromme <mail@bjo3rn.com>
2016-03-25 13:36:51 +0100

    [TASK] Adapt PHP version constraints to TYPO3 7.6.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
   'version' => '2.4.0',
   'constraints' => array (
     'depends' => array(
-      'php' => '5.4.0-5.6.99',
+      'php' => '5.5.0-7.0.99',
       'typo3' => '7.6.0-7.6.99',
     ),
     'conflicts' => array(
