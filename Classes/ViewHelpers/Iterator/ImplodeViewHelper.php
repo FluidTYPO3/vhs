469403f1b317b7470cfde0afba0d662d3816e6d9
Björn Fromme <mail@bjo3rn.com>
2016-05-11 12:11:46 +0200

    [TASK] Override argument descriptions (#1032)

--- Classes/ViewHelpers/Iterator/ImplodeViewHelper.php
+++ Classes/ViewHelpers/Iterator/ImplodeViewHelper.php
@@ -24,4 +24,15 @@ class ImplodeViewHelper extends ExplodeViewHelper {
 	 */
 	protected $method = 'implode';
 
+	/**
+	 * Initialize
+	 *
+	 * @return void
+	 */
+	public function initializeArguments() {
+		parent::initializeArguments();
+		$this->overrideArgument('content', 'string', 'Array or array-convertible object to be imploded by glue', FALSE, NULL);
+		$this->overrideArgument('glue', 'string', 'String used as glue in the content to be imploded. Use glue value of "constant:NAMEOFCONSTANT" (fx "constant:LF" for linefeed as glue)', FALSE, ',');
+	}
+
 }
