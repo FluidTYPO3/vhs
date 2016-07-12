c05a870d9335d31eefb8dc0820c33a50121eef69
Claus Due <claus@namelesscoder.net>
2016-07-12 13:53:03 +0200

    [TASK] Raise maximum compatible TYPO3 version to 8.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -22,7 +22,7 @@ $EM_CONF[$_EXTKEY] = [
   'constraints' => [
     'depends' => [
       'php' => '5.5.0-7.0.99',
-      'typo3' => '7.6.0-8.1.99',
+      'typo3' => '7.6.0-8.2.99',
     ],
     'conflicts' => [],
     'suggests' => [],
