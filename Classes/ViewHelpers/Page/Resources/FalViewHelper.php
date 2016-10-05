0a42f58c7ca7e1b52ef4b9cb1d3108874aea6dfd
monosize <frank.rakow@gmail.com>
2016-10-05 16:08:14 +0200

    [BUGFIX] Slides now working. Missing call to getSlideRecords. Change getSlideRecordsFromPage. to only check if there are valid resources.

--- Classes/ViewHelpers/Page/Resources/FalViewHelper.php
+++ Classes/ViewHelpers/Page/Resources/FalViewHelper.php
@@ -11,6 +11,7 @@ namespace FluidTYPO3\Vhs\ViewHelpers\Page\Resources;
 use FluidTYPO3\Vhs\ViewHelpers\Resource\Record\FalViewHelper as ResourcesFalViewHelper;
 use FluidTYPO3\Vhs\Traits\SlideViewHelperTrait;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Fluid\Core\ViewHelper\Exception;
 use TYPO3\CMS\Frontend\Page\PageRepository;
 
 /**
@@ -56,6 +57,43 @@ class FalViewHelper extends ResourcesFalViewHelper
     }
 
     /**
+     * @return mixed
+     * @throws Exception
+     */
+    public function render()
+    {
+        $record = $this->arguments['record'];
+        $uid = $this->arguments['uid'];
+
+        if (null === $record) {
+            if (null === $uid) {
+                $record = $this->getActiveRecord();
+            } else {
+                $record = $this->getRecord($uid);
+            }
+        }
+        if (null === $record) {
+            throw new Exception('No record was found. The "record" or "uid" argument must be specified.', 1475675124);
+        }
+
+        $record = $this->getSlideRecords($record['uid'], $this->arguments['limit'])[0];
+
+        // attempt to load resources. If any Exceptions happen, transform them to
+        // ViewHelperExceptions which render as an inline text error message.
+        try {
+            $resources = $this->getResources($record);
+        } catch (\Exception $error) {
+            // we are doing the pokemon-thing and catching the very top level
+            // of Exception because the range of Exceptions that are possibly
+            // thrown by the getResources() method in subclasses are not
+            // extended from a shared base class like RuntimeException. Thus,
+            // we are forced to "catch them all" - but we also output them.
+            throw new Exception($error->getMessage(), $error->getCode());
+        }
+        return $this->renderChildrenWithVariableOrReturnInput($resources);
+    }
+
+    /**
      * @param integer $id
      * @return array
      */
@@ -87,10 +125,7 @@ class FalViewHelper extends ResourcesFalViewHelper
     {
         $pageRecord = $this->getRecord($pageUid);
         $resources = $this->getResources($pageRecord);
-        if (null !== $limit && count($resources) > $limit) {
-            $resources = array_slice($resources, 0, $limit);
-        }
-        return $resources;
+        return count($resources) > 0 ? [$pageRecord] : [];
     }
 
     /**
