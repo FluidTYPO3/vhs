bf0fc58f5568492afd8abe89dc24668f7e020795
Claus Due <claus@namelesscoder.net>
2016-11-12 15:07:20 +0100

    [BUGFIX] Fix v:iterator.random with empty input array
    Also converts the VIewHelper to static calling - does
    not change any behavior.

--- Classes/ViewHelpers/Iterator/RandomViewHelper.php
+++ Classes/ViewHelpers/Iterator/RandomViewHelper.php
@@ -10,6 +10,8 @@ namespace FluidTYPO3\Vhs\ViewHelpers\Iterator;
 
 use FluidTYPO3\Vhs\Traits\ArrayConsumingViewHelperTrait;
 use FluidTYPO3\Vhs\Traits\TemplateVariableViewHelperTrait;
+use NamelessCoder\FluidGap\Traits\CompileWithRenderStatic;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3\CMS\Fluid\Core\ViewHelper\Exception;
 
@@ -18,7 +20,7 @@ use TYPO3\CMS\Fluid\Core\ViewHelper\Exception;
  */
 class RandomViewHelper extends AbstractViewHelper
 {
-
+    use CompileWithRenderStatic;
     use TemplateVariableViewHelperTrait;
     use ArrayConsumingViewHelperTrait;
 
@@ -36,15 +38,29 @@ class RandomViewHelper extends AbstractViewHelper
     }
 
     /**
-     * Render method
-     *
-     * @throws Exception
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
      * @return mixed
      */
-    public function render()
-    {
-        $subject = $this->getArgumentFromArgumentsOrTagContentAndConvertToArray('subject');
-        $randomElement = $subject[array_rand($subject)];
-        return $this->renderChildrenWithVariableOrReturnInput($randomElement);
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ) {
+        $subject = static::getArgumentFromArgumentsOrTagContentAndConvertToArrayStatic(
+            $arguments,
+            'subject',
+            $renderChildrenClosure
+        );
+        if (empty($subject)) {
+            return null;
+        }
+        return static::renderChildrenWithVariableOrReturnInputStatic(
+            $subject[array_rand($subject)],
+            $arguments['as'],
+            $renderingContext,
+            $renderChildrenClosure
+        );
     }
 }
