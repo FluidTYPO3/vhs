f47b8678d4dc1ea1ce9cd316909d0d182247e569
Claus Due <claus@namelesscoder.net>
2017-03-05 01:29:49 +0100

    [FEATURE] Raise maximum supported TYPO3 version to 8.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' =>
     array (
       'php' => '7.0.0-7.1.99',
-      'typo3' => '7.6.13-8.5.99',
+      'typo3' => '7.6.13-8.6.99',
     ),
     'conflicts' =>
     array (
