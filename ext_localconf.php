40f8e0339895792321d2259714f2fbeb72114238
Claus Due <claus@namelesscoder.net>
2017-08-26 18:07:51 +0200

    [BUGFIX] Avoid errors if pages.urltype is not in TCA
    The field is removed in TYPO3 dev-master and will not
    exist in the future. By checking if the field exists in
    TCA before adding it to the root line fields, errors
    are prevented.

--- ext_localconf.php
+++ ext_localconf.php
@@ -83,4 +83,6 @@ if (FALSE === is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfi
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['fluid']['namespaces']['v'] = ['FluidTYPO3\\Vhs\\ViewHelpers'];
 
 // add navigtion hide, url and urltype to fix the rendering of external url doktypes
-$GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= (TRUE === empty($GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields']) ? '' : ',') . 'nav_hide,url,urltype';
+if (isset($GLOBALS['TCA']['pages']['columns']['urltype'])) {
+    $GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields'] .= (TRUE === empty($GLOBALS['TYPO3_CONF_VARS']['FE']['addRootLineFields']) ? '' : ',') . 'nav_hide,url,urltype';
+}
