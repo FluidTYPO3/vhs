2c976ef9a082361fa6c6404034e2b8b798ca501f
Claus Due <claus@namelesscoder.net>
2018-03-15 15:01:20 +0100

    [FEATURE] Raise maximum supported TYPO3 version to 9.1.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = array (
     'depends' =>
     array (
       'php' => '7.0.0-7.1.99',
-      'typo3' => '8.7.0-8.7.99',
+      'typo3' => '8.7.0-9.1.99',
     ),
     'conflicts' =>
     array (
