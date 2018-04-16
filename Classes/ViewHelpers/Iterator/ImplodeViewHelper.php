730ce152a20642c8c74b09f12a192f0a3ecbbb89
Moritz Ngo <moritz.ngo@p2media.de>
2018-04-16 10:44:47 +0200

    [TASK] Split ImplodeViewHelper from ExplodeViewHelper
    ImplodeViewHelper should not extend ExplodeViewHelper. Therefore the arguments
    can now be set properly instead of removing them.

--- Classes/ViewHelpers/Iterator/ImplodeViewHelper.php
+++ Classes/ViewHelpers/Iterator/ImplodeViewHelper.php
@@ -8,22 +8,33 @@ namespace FluidTYPO3\Vhs\ViewHelpers\Iterator;
  * LICENSE.md file that was distributed with this source code.
  */
 
+use FluidTYPO3\Vhs\Traits\ArrayConsumingViewHelperTrait;
+use FluidTYPO3\Vhs\Traits\TemplateVariableViewHelperTrait;
 use TYPO3\CMS\Fluid\Core\ViewHelper\Facets\CompilableInterface;
-use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithContentArgumentAndRenderStatic;
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
 
 /**
  * Implode ViewHelper
  *
  * Implodes an array or array-convertible object by $glue.
  */
-class ImplodeViewHelper extends ExplodeViewHelper implements CompilableInterface
+class ImplodeViewHelper extends AbstractViewHelper implements CompilableInterface
 {
-    use CompileWithContentArgumentAndRenderStatic;
+    use CompileWithRenderStatic;
+    use TemplateVariableViewHelperTrait;
+    use ArrayConsumingViewHelperTrait;
 
     /**
-     * @var string
+     * @var boolean
      */
-    protected static $method = 'implode';
+    protected $escapeChildren = false;
+
+    /**
+     * @var boolean
+     */
+    protected $escapeOutput = false;
 
     /**
      * Initialize
@@ -33,8 +44,61 @@ class ImplodeViewHelper extends ExplodeViewHelper implements CompilableInterface
     public function initializeArguments()
     {
         parent::initializeArguments();
-        $this->overrideArgument('content', 'array', 'Array or array-convertible object to be imploded by glue');
-        // `implode()` doesn't have the `limit` argument!
-        unset($this->argumentDefinitions['limit']);
+
+        $this->registerArgument('content', 'array', 'Array or array-convertible object to be imploded by glue');
+        $this->registerArgument(
+            'glue',
+            'string',
+            'String used as glue in the string to be exploded. Use glue value of "constant:NAMEOFCONSTANT" ' .
+            '(fx "constant:LF" for linefeed as glue)',
+            false,
+            ','
+        );
+        $this->registerAsArgument();
+    }
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return mixed
+     */
+    public static function renderStatic(
+        array $arguments,
+        \Closure $renderChildrenClosure,
+        RenderingContextInterface $renderingContext
+    ) {
+        $content = isset($arguments['content']) ? $arguments['content'] : $renderChildrenClosure();
+        $glue = static::resolveGlue($arguments);
+        $output = implode($glue, $content);
+        return static::renderChildrenWithVariableOrReturnInputStatic(
+            $output,
+            $arguments['as'],
+            $renderingContext,
+            $renderChildrenClosure
+        );
+    }
+
+    /**
+     * Detects the proper glue string to use for implode/explode operation
+     *
+     * @param array $arguments
+     * @return string
+     */
+    protected static function resolveGlue(array $arguments)
+    {
+        $glue = $arguments['glue'];
+        if (false !== mb_strpos($glue, ':') && 1 < mb_strlen($glue)) {
+            // glue contains a special type identifier, resolve the actual glue
+            list ($type, $value) = explode(':', $glue);
+            switch ($type) {
+                case 'constant':
+                    $glue = constant($value);
+                    break;
+                default:
+                    $glue = $value;
+            }
+        }
+        return $glue;
     }
 }
