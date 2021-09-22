872d5d65ed1c3aa1808fab8bc3dea319bf7591e8
Vova CHEREDNICHENKO <vovacherednichenko@o-s-i.org>
2021-09-22 11:08:06 +0300

    [BUGFIX] Update PageRepository namespace

--- Classes/ViewHelpers/Page/InfoViewHelper.php
+++ Classes/ViewHelpers/Page/InfoViewHelper.php
@@ -10,7 +10,7 @@ namespace FluidTYPO3\Vhs\ViewHelpers\Page;
 
 use FluidTYPO3\Vhs\Traits\TemplateVariableViewHelperTrait;
 use FluidTYPO3\Vhs\Utility\ErrorUtility;
-use TYPO3\CMS\Frontend\Page\PageRepository;
+use TYPO3\CMS\Core\Domain\Repository\PageRepository;
 use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
 use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
