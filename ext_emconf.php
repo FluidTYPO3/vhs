05bec6cab8e8fba2d29fbed9ee1ab99ab12cd1fe
Claus Due <claus@namelesscoder.net>
2023-01-07 17:09:17 +0100

    [TER] 6.1.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,14 +11,14 @@ $EM_CONF['vhs'] = array (
   'conflicts' => '',
   'priority' => '',
   'module' => '',
-  'state' => 'beta',
+  'state' => 'stable',
   'internal' => '',
   'uploadfolder' => 0,
   'createDirs' => '',
   'modify_tables' => '',
   'clearCacheOnLoad' => 0,
   'lockType' => '',
-  'version' => '6.1.2',
+  'version' => '6.1.3',
   'constraints' =>
   array (
     'depends' =>
