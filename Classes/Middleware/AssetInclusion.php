a2444d5882f2d2806d124c45074bfbe9f440fe58
Claus Due <claus@namelesscoder.net>
2024-01-15 13:12:56 +0100

    [BUGFIX] Sort asset inclusion middleware to execute before content-length
    Close: #1893

--- Classes/Middleware/AssetInclusion.php
+++ Classes/Middleware/AssetInclusion.php
@@ -10,7 +10,6 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use TYPO3\CMS\Core\Http\Stream;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
-use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
 
 class AssetInclusion implements MiddlewareInterface
 {
@@ -37,18 +36,6 @@ class AssetInclusion implements MiddlewareInterface
         $stream = fopen('php://temp', 'rw+');
         fputs($stream, $contents);
 
-        $response = $response->withBody(new Stream($stream));
-
-        // Copied from \TYPO3\CMS\Frontend\Middleware\ContentLengthResponseHeader to ensure proper content-length.
-        if ($GLOBALS['TSFE'] instanceof TypoScriptFrontendController) {
-            if ((!isset($GLOBALS['TSFE']->config['config']['enableContentLengthHeader'])
-                    || $GLOBALS['TSFE']->config['config']['enableContentLengthHeader'])
-                && !$GLOBALS['TSFE']->isBackendUserLoggedIn() && !($GLOBALS['TYPO3_CONF_VARS']['FE']['debug'] ?? false)
-                && !($GLOBALS['TSFE']->config['config']['debug'] ?? false) && !$GLOBALS['TSFE']->doWorkspacePreview()
-            ) {
-                $response = $response->withHeader('Content-Length', (string)$response->getBody()->getSize());
-            }
-        }
-        return $response;
+        return $response->withBody(new Stream($stream));
     }
 }
--- Configuration/RequestMiddlewares.php
+++ Configuration/RequestMiddlewares.php
@@ -4,13 +4,7 @@ return [
         'fluidtypo3/vhs/asset-inclusion' => [
             'target' => \FluidTYPO3\Vhs\Middleware\AssetInclusion::class,
             'after' => [
-                'typo3/cms-frontend/shortcut-and-mountpoint-redirect',
-                'typo3/cms-frontend/prepare-tsfe-rendering',
-                'fluidtypo3/vhs/request-availability',
-            ],
-            'before' => [
                 'typo3/cms-frontend/content-length-headers',
-                'typo3/cms-frontend/output-compression',
             ],
         ],
         'fluidtypo3/vhs/request-availability' => [
