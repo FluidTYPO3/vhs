8863735fb7ddf4ec4cd15ff08fb2519a9be5003d
Christian Weiske <weiske@mogic.com>
2024-09-05 13:14:17 +0200

    [DOC] Update VH docs for v:content.get and v:resource.record (#1912)

--- Classes/ViewHelpers/Resource/RecordViewHelper.php
+++ Classes/ViewHelpers/Resource/RecordViewHelper.php
@@ -11,7 +11,11 @@ namespace FluidTYPO3\Vhs\ViewHelpers\Resource;
 use FluidTYPO3\Vhs\ViewHelpers\Resource\Record\AbstractRecordResourceViewHelper;
 
 /**
- * Generic FAL resource ViewHelper
+ * Generic FAL resource ViewHelper.
+ *
+ * .. warning::
+ *    This view helper must only be used with FAL resources.
+ *    It is **not** meant to be used as generic data loading view helper.
  */
 class RecordViewHelper extends AbstractRecordResourceViewHelper
 {
