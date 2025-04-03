3df9c3472ce5613e2c53f7415dd60ea61c2b852a
Claus Due <claus@namelesscoder.net>
2025-04-03 16:03:03 +0200

    [TER] 7.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,14 +11,14 @@ $EM_CONF['vhs'] = array (
   'conflicts' => '',
   'priority' => '',
   'module' => '',
-  'state' => 'beta',
+  'state' => 'stable',
   'internal' => '',
   'uploadfolder' => 0,
   'createDirs' => '',
   'modify_tables' => '',
   'clearCacheOnLoad' => 0,
   'lockType' => '',
-  'version' => '7.1.0',
+  'version' => '7.1.1',
   'constraints' => 
   array (
     'depends' => 
