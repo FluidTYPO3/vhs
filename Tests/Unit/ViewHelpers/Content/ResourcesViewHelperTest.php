cf1537955791abfbd6c20f727865242b74a63614
Claus Due <claus@namelesscoder.net>
2026-06-29 12:22:36 +0200

    [REMOVAL] Make resource ViewHelpers demand uid or record
    FAL etc. resource ViewHelpers no longer allow calling without both
    record and uid arguments, which previously would have attempted
    to resolve the current record data from ContentObectRenderer.
    
    Calling these ViewHelpers must now include either record or uid as
    arguments. Argument descriptions updated accordingly.
    
    Close: #1963

--- Tests/Unit/ViewHelpers/Content/ResourcesViewHelperTest.php
+++ Tests/Unit/ViewHelpers/Content/ResourcesViewHelperTest.php
@@ -16,6 +16,11 @@ use FluidTYPO3\Vhs\Tests\Unit\ViewHelpers\AbstractViewHelperTestCase;
  */
 class ResourcesViewHelperTest extends AbstractViewHelperTestCase
 {
+    protected array $defaultMockMethods = [
+        'getRecord',
+        'getResources',
+    ];
+
     public function testRenderFailsWithoutFieldArgument()
     {
         $this->expectViewHelperException();
