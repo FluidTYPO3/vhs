2953534c20bf5e27f957a97929ef71b574f5836a
Claus Due <claus@namelesscoder.net>
2026-06-29 14:03:40 +0200

    [TASK] Remove phpstan bootstrap

--- phpstan-bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('LF', "\n");
-define('TYPO3_MODE', 'CLI');
-define('TYPO3_REQUESTTYPE', 1);
-define('TYPO3_REQUESTTYPE_FE', 1);
-define('TYPO3_REQUESTTYPE_CLI', 3);
-define('TYPO3_version', '9.5.31');
