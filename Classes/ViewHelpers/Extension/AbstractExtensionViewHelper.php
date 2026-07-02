01283ce2edee155b80a968f9f9b3701299a631b6
Claus Due <claus@namelesscoder.net>
2026-07-02 13:39:10 +0200

    [TASK] Fix an outdated error message

--- Classes/ViewHelpers/Extension/AbstractExtensionViewHelper.php
+++ Classes/ViewHelpers/Extension/AbstractExtensionViewHelper.php
@@ -48,7 +48,7 @@ abstract class AbstractExtensionViewHelper extends AbstractViewHelper
         $extensionName = RequestResolver::resolveControllerExtensionNameFromRenderingContext($renderingContext);
         if (empty($extensionName)) {
             throw new \RuntimeException(
-                'Unable to read extension name from ControllerContext and value not manually specified',
+                'Unable to read extension name from RenderingContext and value not manually specified',
                 1364167519
             );
         }
