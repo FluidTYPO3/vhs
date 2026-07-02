7ad50178d567912b84e2e45a2337cc493e71f20e
Claus Due <claus@namelesscoder.net>
2026-07-02 14:35:15 +0200

    [REMOVAL] Remove support for PHP <8.3

--- ext_emconf.php
+++ ext_emconf.php
@@ -23,7 +23,7 @@ $EM_CONF['vhs'] = array (
   array (
     'depends' =>
     array (
-      'php' => '7.4.0-8.3.99',
+      'php' => '8.3.99-8.5.99',
       'typo3' => '12.4.0-13.4.99',
     ),
     'conflicts' =>
